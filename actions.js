2d8bc1e4bec93f26ddb807e731dfca975c119000
Carrie M <carriem@thecommunityus.com>
2021-04-14 22:29:39 -0400

    Added array of topics

--- actions.js
+++ actions.js
@@ -1,5 +1,17 @@
 const getTopicButton = document.querySelector('.submit-button');
 const topic = document.querySelector('.topic-answer').innerHTML;
+const allTopics = [
+    "With creativity, where does the tech fit in?",
+    "NFTs, we just sold this panel",
+    "AR vs VR in the battle beyond reality",
+    "What will experiences post COVID look like?",
+    "Does the internet really break?",
+    "Blockchain = Web 3.0",
+    "Options on options, what technology is right?",
+    "Are we really talking right now or are we deep fakes?",
+    "Gaming FTW",
+    "AI and Humans: underrated or overrated"
+];
 
 getTopicButton.addEventListener('click', getTopic);
 
@@ -9,3 +21,4 @@ function getTopic() {
 
 console.log(getTopicButton);
 console.log(topic);
+console.log(allTopics);
